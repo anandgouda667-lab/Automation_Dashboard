c9f17b2be82cec331bb320922c0611b4a9c126ea
Anand Kumar <anandgouda667@gmail.com>
2026-04-02 16:05:54 +0530

    Updated CI pipeline

--- src/test/java/com/SeleniumDashboard/Locators2.java
+++ src/test/java/com/SeleniumDashboard/Locators2.java
@@ -1,36 +1,70 @@
 package com.SeleniumDashboard;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
-
+import org.testng.Assert;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import junit.framework.Assert;
+
 
 public class Locators2 {
-	public static void main(String[] args) throws InterruptedException {
-		
+	@Test
+	public void loginTest() throws IOException {
+
 		WebDriverManager.chromedriver().setup();
-		
-		WebDriver driver = new ChromeDriver();
-		
-		driver.manage().window().maximize();
-		
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless"); // important for CI
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+
+		WebDriver driver = new ChromeDriver(options);
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		driver.get("https://rahulshettyacademy.com/locatorspractice/");
-		
-		driver.findElement(By.id("inputUsername")).sendKeys("Anand");
-		
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
-		
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		Thread.sleep(3000);
-		
-		System.out.println(driver.findElement(By.tagName("p")).getText());
-		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+
+		try {
+
+			driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+			driver.findElement(By.id("inputUsername")).sendKeys("Anand");
+			driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+			driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+			String message = wait.until(
+					ExpectedConditions.visibilityOfElementLocated(
+							By.cssSelector("div[class='login-container'] p"))
+					).getText();
+
+			System.out.println("Message: " + message);
+
+			Assert.assertEquals(message, "You are successfully logged in.");
+		} catch (AssertionError | Exception e) {
+
+			// 📸 Take screenshot if test fails
+			takeScreenshot(driver, "loginTest");
+
+			throw e; // rethrow so TestNG marks test as FAILED
+		} finally {
+			driver.quit(); // always close browser
+		}
+	}
+	public static void takeScreenshot(WebDriver driver, String testName) throws IOException {
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File dest = new File("screenshots/" + testName + ".png");
+		FileUtils.copyFile(src, dest);
 		driver.quit();
 	}
 
